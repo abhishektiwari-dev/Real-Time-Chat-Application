a561ff9d605c882512ad58af35dbfe93fc08e2bf
abhishektiwari-dsa <heyabhishek20@gmail.com>
2026-03-13 14:18:27 +0530

    RTCA

---
+++ src/main/java/com/chat/app/controller/ChatController.java
@@ -0,0 +1,30 @@
+package com.chat.app.controller;
+
+
+import com.chat.app.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+
+    @GetMapping("chat")
+    public String chat(){
+        return "chat";
+    }
+
+    @MessageMapping("/typing")
+    @SendTo("/topic/typing")
+    public String typing(String username) {
+        return username;
+    }
+
+}
